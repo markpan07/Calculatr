849ff81df31956f50906cee1134bf86b4cc2b895
МАРК <markpan07@mail.ru>
2023-03-19 18:44:22 +0300

    HW is done

--- src/main/java/pro/sky/calculatr/controller/CalculatorService.java
+++ src/main/java/pro/sky/calculatr/controller/CalculatorService.java
@@ -5,5 +5,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculatorService {
 
+    public double plus(double a, double b) {
+        return a + b;
+    }
 
+    public double minus(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
 }
